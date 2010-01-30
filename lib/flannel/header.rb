0031c1c0745c439e6092f25f91cb586c09d3e391
rubyyot <jamal.hansen@gmail.com>
2010-01-30 10:01:44 -0600

    added id to header

--- lib/flannel/header.rb
+++ lib/flannel/header.rb
@@ -13,10 +13,10 @@ module Flannel
     
     def parse text
       # need to check for leading whitespace
-      leader, @text = split_leader text
-      
+      leader, text = split_leader text
+
       @level = calculate_level leader
-      @identity = @text.gsub(%r{[^\w\s\_]},'').gsub(%r{[^\w]},' ').split.join('_').downcase.to_sym
+      get_attributes text
     end
     
     def split_leader text
@@ -24,11 +24,25 @@ module Flannel
       text.split(/(?=\w)/, 2)
     end
     
+    def get_attributes text
+      @text, identity = text.split(/\#/, 2)
+
+      @identity ||= symbolize( identity ? identity : @text)
+    end
+      
+    def symbolize text
+      text.gsub(%r{[^\w\s\_]},'').gsub(%r{[^\w]},' ').split.join('_').downcase.to_sym
+    end
+    
     def calculate_level leader
-      return 0 unless leader
+      return 1 unless leader
       
       tabs = leader.length - leader.gsub(/\t/, "").length
-      ((leader.length - tabs) / 2) + tabs  # tabs count for 2 spaces    
+      
+      level = (((leader.length - tabs) / 2) + tabs) + 1  # tabs count for 2 spaces, starting at 1 max 6  
+      level = 1 if level < 1
+      level = 6 if level > 6
+      level
     end
   end
 end
--- test/header_test.rb
+++ test/header_test.rb
@@ -3,6 +3,24 @@ require 'test_helper'
 class HeaderTest < Test::Unit::TestCase
   context "basic properties" do
     setup do
+      @header = Flannel::Header.new "My Header"
+    end
+    
+    should "have text of My Header" do
+      assert_equal "My Header", @header.text
+    end
+    
+    should "have identity of :my_header" do
+      assert_equal :my_header, @header.identity
+    end
+    
+    should "have level of 1" do
+      assert_equal 1, @header.level
+    end
+  end
+  
+  context "space indented header" do
+    setup do
       @header = Flannel::Header.new "    My Header"
     end
     
@@ -14,8 +32,8 @@ class HeaderTest < Test::Unit::TestCase
       assert_equal :my_header, @header.identity
     end
     
-    should "have level of 2" do
-      assert_equal 2, @header.level
+    should "have level of 3" do
+      assert_equal 3, @header.level
     end
   end
   
@@ -32,8 +50,30 @@ class HeaderTest < Test::Unit::TestCase
       assert_equal :my_header, @header.identity
     end
     
-    should "have level of 2" do
-      assert_equal 2, @header.level
+    should "have level of 3" do
+      assert_equal 3, @header.level
     end
+  end
+  
+  context "identity" do
+    should "be created from the text" do
+      header = Flannel::Header.new "My Header"
+      assert_equal :my_header, header.identity
+    end
+    
+    should "remove non-standard characters" do
+      header = Flannel::Header.new "My !@%^&* Kick*** Header"
+      assert_equal :my_kick_header, header.identity
+    end
+    
+    should "be overridden by # syntax" do
+      header = Flannel::Header.new "My Header#your header"
+      assert_equal :your_header, header.identity
+    end
+  end
+  
+  should "not have a level > 6" do
+    header = Flannel::Header.new "\t\t\t\t\t\t\t\t\t\t\t\tMy Header"
+    assert_equal 6, header.level
   end
 end
