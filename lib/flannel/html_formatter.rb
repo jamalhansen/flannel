3083984dd058b9a0a3276d230347bb15c4b9a908
rubyyot <jamal.hansen@gmail.com>
2010-02-28 21:20:59 -0600

    forcing encoding to UTF-8

--- lib/flannel/html_formatter.rb
+++ lib/flannel/html_formatter.rb
@@ -1,37 +1,39 @@
 require 'flannel/html_transformable'
 
 module Flannel
-  
+
   # HtmlFormatter is responsible for formatting text blocks as html
   class HtmlFormatter
     include Flannel::HtmlTransformable
-    
+
     def initialize
-      @tags ={:preformatted => "pre", 
-              :feed => "ul", 
-              :list => "ul", 
-              :dlist => "dl", 
+      @tags ={:preformatted => "pre",
+              :feed => "ul",
+              :list => "ul",
+              :dlist => "dl",
               :header_one => "h1", #new style
-              :header_two => "h2", 
-              :header_three => "h3", 
-              :header_four => "h4", 
-              :header_five => "h5", 
+              :header_two => "h2",
+              :header_three => "h3",
+              :header_four => "h4",
+              :header_five => "h5",
               :header_six => "h6",
               :paragraph => "p",
               :blockquote => "blockquote"}
     end
-    
+
     def do text, style, id=nil
       @text = text
       @style = style
-      @id = id      
-      
-      build_html
+      @id = id
+
+      html = build_html
+      html.force_encoding("UTF-8")
+      html
     end
 
     def build_html
       case @style
-	
+
       when :preformatted
         html =  html_escape @text
       when :feed
@@ -41,16 +43,16 @@ module Flannel
       else
         html =  build_wiki_links @text
         html =  convert_external_links html
-        
+
         if @style == :list
           html = format_list html
         end
-        
+
         if @style == :dlist
           html =  format_dlist html
         end
       end
-      
+
       wrap(html, @tags[@style], @id)
     end
   end
--- test/base_block_test.rb
+++ test/base_block_test.rb
@@ -3,53 +3,67 @@ require 'test_helper'
 class BaseBlockTest < Test::Unit::TestCase
   def test_initializes_with_header_array_and_text
     block = new_block [[[:block_type, :paragraph], [:block_id, "some-id"], [:attribute_list]], "some text"]
-    
+
     assert_equal :paragraph, block.type
     assert_equal "some-id", block.id
     assert_equal "some text", block.text
   end
-  
+
   def test_initializes_with_parent_id
     block = new_block [[[:block_type, :paragraph], [:block_id, "some-id"], [:parent_id, "parent-id"]], "some text"]
-    
+
     assert_equal :paragraph, block.type
     assert_equal "some-id", block.id
     assert_equal "parent-id", block.parent_id
     assert_equal "some text", block.text
   end
-  
+
   def test_initializes_with_attribute_list
     block = new_block [[[:block_type, :paragraph], [:block_id, "some-id"], [:attribute_list, [:class, "cool"]]], "some text"]
-    
+
     assert_equal :paragraph, block.type
     assert_equal "some-id", block.id
     assert_equal "cool", block.attributes[:class]
     assert_equal "some text", block.text
   end
-  
+
+  def test_stores_in_utf_8
+    text = "some text"
+    text.encode("US-ASCII")
+    block = new_block [[[:block_type, :paragraph], [:block_id, "some-id"], [:attribute_list]], text]
+    assert_equal Encoding::UTF_8, block.text.encoding
+  end
+
+  def test_outputs_in_utf_8
+    text = "some text"
+    text.encode("US-ASCII")
+    block = new_block [[[:block_type, :paragraph], [:block_id, "some-id"], [:attribute_list]], text]
+    assert_equal Encoding::UTF_8, block.to_h.encoding
+  end
+
   def test_initializes_with_attribute_list_and_parent_id
     block = new_block [[[:block_type, :paragraph], [:block_id, "some-id"], [:parent_id, "parent-id"], [:attribute_list, [:class, "cool"]]], "some text"]
-   
+
     assert_equal :paragraph, block.type
     assert_equal "some-id", block.id
     assert_equal "parent-id", block.parent_id
     assert_equal "cool", block.attributes[:class]
     assert_equal "some text", block.text
   end
-  
+
   def test_initializes_with_header_array_and_no_text
     block = new_block [[[:block_type, :paragraph], [:block_id, "some-id"]]]
-    
+
     assert_equal :paragraph, block.type
     assert_equal "some-id", block.id
     assert_nil block.text, "Block#text should be nil"
   end
-  
+
   def test_initialized_from_parser_output
     text = ":preformatted my-code great-code class=ruby whiz=bang:\ndef foo arg\n\t puts arg\n end"
     doc = BlockParser.new.parse(text)
     block = Flannel::BaseBlock.new doc.content[0]
-   
+
     assert_equal :preformatted, block.type
     assert_equal "my-code", block.id
     assert_equal "great-code", block.parent_id
