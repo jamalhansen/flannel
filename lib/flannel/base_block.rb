67c2a5b782fbd26e73a4b4ace4d6c7a7bcf4e780
rubyyot <jamal.hansen@gmail.com>
2010-02-28 21:04:15 -0600

    forcing UTF-8

--- lib/flannel/base_block.rb
+++ lib/flannel/base_block.rb
@@ -4,26 +4,26 @@ require 'flannel/html_formatter'
 module Flannel
   class BaseBlock
     attr_reader :type, :id, :text, :parent_id, :attributes
-    
+
     def initialize block
       form = block[0]
-      
+
       if form == :block
         create_from_list block[1]
       else
         @text = block[1]
         @type = :paragraph
       end
-      
+
       strip_and_encode_text
     end
-    
+
     def create_from_list list
       header = list.shift
       @type = header.shift[1]
       @id = header.shift[1]
       @attributes = {}
-      
+
       next_item = header.shift
       while next_item
         case next_item[0]
@@ -34,24 +34,25 @@ module Flannel
           next_item.each do |attribute|
             @attributes[attribute[0]] = attribute[1]
           end
-        end  
+        end
         next_item = header.shift
       end
-      
+
       @text = list.shift
     end
-    
+
     def to_h
       html_formatter = Flannel::HtmlFormatter.new
       html_formatter.do(@text, @type, @id)
     end
-    
+
     def strip_and_encode_text
-      @text.encode("UTF-8")
-      
+      return unless @text
+      @text.force_encoding("UTF-8")
+
       return if @type == :preformatted
       return unless @text
-      
+
       @text = @text.strip
     end
   end
